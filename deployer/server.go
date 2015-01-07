b4f9ca5145d200dcc8f091dfdac859ba7b616c2e
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-01-07 18:37:48 +0900

    deployer: Scrub out all UUIDs with CleanUUID().
    Signed-off-by: Blaž Hrastnik <blaz.hrast@gmail.com>

--- deployer/server.go
+++ deployer/server.go
@@ -201,6 +201,9 @@ func createDeployment(w http.ResponseWriter, req *http.Request, params httproute
 		http.Error(w, err.Error(), 500)
 		return
 	}
+	deployment.ID = postgres.CleanUUID(deployment.ID)
+	deployment.OldReleaseID = postgres.CleanUUID(deployment.OldReleaseID)
+	deployment.NewReleaseID = postgres.CleanUUID(deployment.NewReleaseID)
 
 	args, err := json.Marshal(deployID{ID: deployment.ID})
 	if err != nil {
@@ -214,7 +217,6 @@ func createDeployment(w http.ResponseWriter, req *http.Request, params httproute
 		http.Error(w, err.Error(), 500)
 		return
 	}
-	deployment.ID = postgres.CleanUUID(deployment.ID)
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(deployment); err != nil {
 		http.Error(w, err.Error(), 500)
